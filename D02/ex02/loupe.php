bb95b3a4b9d46086871c7a7394ece51de0650ea0
Pablo CLEMENT <pclement@e2r10p6.42.fr>
2018-03-28 19:36:11 +0200

    D02

--- D02/ex02/loupe.php
+++ D02/ex02/loupe.php
@@ -1,25 +1,24 @@
 #!/usr/bin/php
 <?php
 
+function ft_match ($matches)
+{
+	$str = strtoupper($matches[2]);
+	return $matches[1].$str.$matches[3];
+}
+
 	if ($argc < 2 || !file_exists($argv[1]))
 		exit();
 	$fd = fopen($argv[1], 'r');
-	echo $fd;
 	$str = "";
     while (($buffer = fgets($fd)) && !feof($fd)) {
-        $str = $str.fgets($fd);
+        $str = $str.$buffer;
 	}
 	fclose($fd);
-	echo "new fd = ";
 
-echo "$str\n";
-	$str = '<html><head><title>Nice page</title></head>
-<body>Hello World <a href=http://cyan.com title="un lien">Ceci est un lien</a>
-<br /><a href=http://www.riven.com> Et ca aussi <img src=wrong.image title="et encore ca"></a>
-</body></html>';
-preg_replace_callback('/<a.* title="(.*)\"*>/', function ($matches){strtoupper($matches[1]);}, $str);
-preg_replace_callback('/<a.*>(.*)<\/a>/', function ($matches){strtoupper($matches[1]);}, $str);
-
+//	$str = preg_replace_callback('/(<a.* title=")(.*)(".*>)/', 'ft_match', $str);
+	$str = preg_replace_callback('/(<a.*?>)(.*)(<\/a>)/', 'ft_match', $str);
+ 
 
 echo "$str\n";
 
