253db3185bd5ab3829f8103801c626edf570c7e0
Pablo CLEMENT <pclement@e3r10p6.42.fr>
2018-03-29 12:11:30 +0200

    modifs on who

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -2,19 +2,22 @@
 <?php
 if (!($file = fopen("/var/run/utmpx", "r")))
 	exit();
+$array=[];
 while ($utmpx = fread($file, 628))
 {
-	$unpack = unpack("a256a/a4b/a32c/id/ie/I2f/a256g/i16h", $utmpx);
-	$array[$unpack['c']] = $unpack;
+	$unpack = unpack("a256Name/a4b/a32Sys/id/iType/I2Date", $utmpx);
+	$array[$unpack['Sys']] = $unpack;
 }
+sort($array);
+
 $str="";
 date_default_timezone_set('Europe/Paris');
-foreach ($array as $login)
+foreach ($array as $entry)
 {
-    if ($login['e'] === 7)
+    if ($entry['Type'] === 7)
 	{
-		$str = $str.str_pad(substr(trim($login['a']), 0, 8), 9, " ").str_pad(substr(trim($login['c']), 0, 8), 9, " ");
-		$date = date("M d H:i\n", $login['f1']);
+		$str = $str.str_pad(substr(trim($entry['Name']), 0, 8), 9, " ").str_pad(substr(trim($entry['Sys']), 0, 8), 9, " ");
+		$date = date("M d H:i\n", $entry['Date1']);
 		$str = $str.$date;
     }
 }
