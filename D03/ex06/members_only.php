11bbb2f75645967ef8255d7e775466325d136d3e
blopax <pablo42clement@gmail.com>
2018-03-30 11:56:02 +0200

    d03 - ex06

--- D03/ex06/members_only.php
+++ D03/ex06/members_only.php
@@ -1,12 +1,12 @@
 <?php
-echo "bob";
-print_r($_SERVER);
-if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys")
+if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys")
 {
 	$img = base64_encode(file_get_contents('../img/42.png'));
 	echo "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64,$img'>\n</body></html>\n";
 }
 else
 {
-	echo "<html><body>Cette zone est accessible uniiquement aux membres du site</body></html>";
+	header("http/1.0 401 Unauthorized");
+	header("WWW-Authenticate: basic realm=''Espave membres''");
+	echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
 ?>
