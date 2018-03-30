a3dbc4dffb7778d6153fe7252067e7134c6c884f
Pablo CLEMENT <pclement@e1r6p6.42.fr>
2018-03-30 15:03:16 +0200

    add isset

--- D03/ex03/cookie_crisp.php
+++ D03/ex03/cookie_crisp.php
@@ -1,8 +1,11 @@
 <?php
-if ($_GET["action"] == "set" && $_GET['name'])
-	setcookie($_GET["name"], $_GET["value"], time() + 3600);
-if ($_GET["action"] == "get" && $_COOKIE[$_GET['name']])
-	echo $_COOKIE[$_GET['name']]."\n";
-if ($_GET["action"] == "del" && $_GET['name'])
-	setcookie($_GET["name"], $_GET["value"], time() - 1);
+if (isset($_GET["action"]))
+{
+	if ($_GET["action"] == "set" && isset($_GET['name']) && isset($_GET['value']))
+		setcookie($_GET["name"], $_GET["value"], time() + 3600);
+	if ($_GET["action"] == "get" && isset($_COOKIE[$_GET['name']]))
+		echo $_COOKIE[$_GET['name']]."\n";
+	if ($_GET["action"] == "del" && isset($_GET['name']))
+		setcookie($_GET["name"], '' , time() - 1);
+}
 ?>
